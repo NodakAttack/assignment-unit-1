b13a74ca0c517a8ff794b904f41c8a345652c5f6
NodakAttack <125848776+NodakAttack@users.noreply.github.com>
2023-06-14 01:20:29 -0500

    sourced in js as helloworld

---
+++ assignment/helloWorld.js
@@ -0,0 +1,30 @@
+console.log("hello");
+
+let x = 1;
+
+let tempRightNow = 81;
+
+console.log("gaming" , tempRightNow = tempRightNow + 1);
+
+tempRightNow += 1;
+
+tempRightNow ++;
+
+console.log(tempRightNow);
+
+let game = "zelda";
+let title = "TotK";
+game = game + ' ' + title;
+console.log('game' , game);
+
+let isCloudy = false;
+
+let toReplace = document.querySelector('#fun2');
+
+toReplace.innerHTML = tempLater;
+
+if (isCloudy === false) {
+    console.log('clear skys.');
+} else {
+    console.log('rain day');
+}
